e95ba1e0263a172f2cc838b9b10212e47658c701
Chandan Gautam <chandangautam9799@gmail.com>
2026-06-22 23:29:03 +0530

    Time: 0 ms (100%), Space: 76.6 MB (9.74%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n=arr.size();
+        int mx=-1;
+        vector<int>ans;
+        for(int i=n-1; i>=0; i--){
+            ans.push_back(mx);
+            mx=max(mx,arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
