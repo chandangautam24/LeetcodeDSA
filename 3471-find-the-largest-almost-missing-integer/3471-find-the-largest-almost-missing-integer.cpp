3c87edd4c282c0d572db2ddd33e726584af86052
Chandan Gautam <chandangautam9799@gmail.com>
2026-08-19 00:52:24 +0530

    Time: 0 ms (100%), Space: 29.4 MB (51.5%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n = nums.size();
-        int l = 0, r = k-1;
-        unordered_map<int, int> mpp;
-        int ans = -1;
-        if (k == 1) {
-             for (int i = 0; i < n; i++) {
-            mpp[nums[i]]++;
-        }
-            for (auto j : mpp) {
-                if (j.second == 1) {
-                    ans = max(ans, j.first);
+        int n=nums.size();
+        if(k==1){
+            unordered_map<int,int>mpp;
+            for(int i=0; i<n; i++){
+                mpp[nums[i]]++;
+            }
+            int ans=-1;
+            for(auto [x,cnt]: mpp){
+                if(cnt==1){
+                    ans=max(ans,x);
                 }
             }
             return ans;
         }
-        if (k == n) {
-            ans = *max_element(nums.begin(), nums.end());
-            return ans;
+        if(k==n){
+            return *max_element(nums.begin(),nums.end());
         }
-        while(r<n){
-         for(int i=l; i<=r; i++){
-           mpp[nums[i]]++;
-         }
-         l++;
-         r++;
+        unordered_map<int,int>mpp;
+        int ans=-1;
+        for(int i=0; i<n; i++){
+            mpp[nums[i]]++;
         }
-        for(auto x:mpp){
-            if(x.second==1){
-                ans=max(ans,x.first);
-            }
+        if(mpp[nums[0]]==1){
+          ans=max(ans,nums[0]);
+        }
+        if(mpp[nums[n-1]]==1){
+          ans=max(ans,nums[n-1]);
         }
         return ans;
     }
